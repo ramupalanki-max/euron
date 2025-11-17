5659a406d615fd2eaacf5deec5c48a3d05f3e63b
Ramu Palanki <rpalanki@opentext.com>
2025-11-17 09:29:37 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["euron"]
+euron_data = db["euron_coll"]
+
+app = FastAPI()
+
+class eurondata(BaseModel):
+    name: str
+    phone: int
+    city: str
+    course: str
+    
+@app.post("/euron/insert")    
+async def euron_data_insert_helper(data:eurondata):
+    result  = await euron_data.insert_one(data.dict())
+    return str(result.inserted_id)
+
+def euron_helper(doc):
+    doc["id"] = str(doc["_id"])
+    del doc["_id"]
+    return doc
+
+
+@app.get("/euron/getdata")
+async def get_euron_data():
+    iterms = []
+    cursor = euron_data.find({})
+    async for document in cursor:
+        iterms.append(euron_helper(document))
+    return iterms
